a3b2e9ce4d7d22e702dd0ab25be7aea30199edff
Prakash K <kprakash19940625@gmail.com>
2016-04-04 16:53:24 +0530

    Update TestEnvTestCases.java

--- TestEnvTestCases.java
+++ TestEnvTestCases.java
@@ -16,28 +16,28 @@ public class TestEnvTestCases {
  @Test
  public void bothgivencorrect(){
   Login n=new Login("1","1");
-  assertEquals("Login Failed.",n.validate());
+  assertEquals("Login Failed",n.validate());
  }
  @Test
  public void bothgivenwrong(){
   Login n=new Login("username","password");
-  assertEquals("Login Failed.",n.validate());
+  assertEquals("Login Failed",n.validate());
  }
  @Test
  public void bothareOmitted(){
   Login n=new Login("","");
-  assertEquals("Login Failed.",n.validate());
+  assertEquals("Login Failed",n.validate());
   
  }
  @Test
  public void usernameOmitted(){
   Login n=new Login("","password");
-  assertEquals("Login Failed.",n.validate());
+  assertEquals("Login Failed",n.validate());
  }
  @Test
  public void passwordOmitted(){
   Login n=new Login("username","");
-  assertEquals("Login Failed.",n.validate());
+  assertEquals("Login Failed",n.validate());
  }
  @Test
  public void wrongusername(){
