e4316ae9f07e9db9d401c58091e021ab92d005fd
M R Siva Prasad <mrsivaprasad@users.noreply.github.com>
2016-09-15 13:00:23 +0530

    Update percentage.py

--- percentage.py
+++ percentage.py
@@ -1,5 +1,9 @@
 import os
 #This file generates the test percentage ratio of TEST_CASES.
+def functionName( percentage ):
+    if percentage < 70.0:
+        raise Exception("BUILD FAILED :" +str(percentage))
+
 text_file=open("TEST-TestEnvTestCases.txt","r")
 text_file.readline();
 data= text_file.readline();
@@ -11,15 +15,11 @@ for i in indexs:
     f.pop();
     s=''.join(f);
     tests_data.append(int(s));
-
 tests_run=int(tests_data[0]);
 tests_fail=int(tests_data[1]);
 tests_success=tests_run-tests_fail;
 tests_data.append(tests_success);
 tests_perc=(float(tests_success)/(tests_run))*100.00;
 tests_data.append(tests_perc);
-
-
-print tests_data[-1];
-
+functionName(tests_data[-1]);
 text_file.close()
